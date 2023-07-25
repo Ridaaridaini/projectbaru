59f98b4c47b4ebbb728349df6b147511ece946c6
friskaalfanda <friska.alfanda5@gmail.com>
2023-07-25 21:47:54 +0700

    Tugas1

---
+++ Friska/Logic324/src/day1/latihan/Soal12.java
@@ -0,0 +1,14 @@
+package day1.latihan;
+
+public class Soal12 {
+    public static void main(String[] args) {
+        int a =5;
+        int b = 2;
+        float c = (float) a;
+        float d = (float) b;
+        float hasilbagi = a/b;
+        System.out.println("Hasil Bagi dari kedua bilangan tersebut adalah: "+ hasilbagi);
+
+
+    }
+}
