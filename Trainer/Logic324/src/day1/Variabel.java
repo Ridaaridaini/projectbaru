4c9d664707ee7de5df5d893c1fe5a0bbb05cb146
yuhan <yuhan@gmail.com>
2023-07-25 16:47:14 +0700

    Day 1

---
+++ Trainer/Logic324/src/day1/Variabel.java
@@ -0,0 +1,44 @@
+package day1;
+
+public class Variabel {
+    public static void main(String[] args) {
+        //Tipe Data Primitif
+            //Bilangan bulat
+                //byte
+                //short
+                //integer
+                //long
+        //Deklarasi variabel
+        byte bilByte;
+        short bilShort;
+        int bilInt;
+        long bilLong;
+
+        //Assignment variabel
+        bilByte = 1;
+        bilShort = 1 + 3 + 6;
+        bilInt = 1;
+        bilLong = 1000000000000000l;
+
+            //Bilangan desimal
+                //float
+                //double
+
+        //Deklarasi & Inisiasi
+        float bilFloat = 1.5f;
+        double bilDouble = 2.5;
+
+            //Boolean
+        boolean boTrue = true;
+        boolean boFalse = false;
+
+            //Char
+        char mChar = 'A';
+        char mChar2 = 65; //Menganut tabel ASCII
+        System.out.println(mChar);
+
+        //Tipe Data Non Primitif
+            //String
+        String nama = "Yuhan";
+    }
+}
