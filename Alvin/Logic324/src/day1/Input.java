08a4b07e50c88b7004661a2635213f1707ae6371
Aslamadin Alvian Haz <alvianaslamadin@gmail.com>
2023-07-25 11:38:13 +0700

    Belajar print, variabel, stdin/stdout
    Learning

---
+++ Alvin/Logic324/src/day1/Input.java
@@ -0,0 +1,26 @@
+package day1;
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        //instansiasi
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("inputkan nama: ");
+        String myName =  input.nextLine();
+        System.out.println("umur: ");
+        int myUmur = input.nextInt();
+        input.close();
+
+        Date tahun = new Date();
+        int date = tahun.getYear();
+
+        int tahunLahir = date-myUmur;
+
+        System.out.println("nama saya "+myName);
+        System.out.println("umur saya "+myUmur);
+        System.out.println("tahun lahir "+tahunLahir);
+    }
+}
