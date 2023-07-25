9b4b47876dc3ac6a13406d6a045836877feb4c54
Dianty99 <diantyfistikaap@gmail.com>
2023-07-25 16:52:44 +0700

    Jawaban Latihan

---
+++ Danty/Logic324/src/day1/latihan/Soal12.java
@@ -0,0 +1,13 @@
+package day1.latihan;
+
+public class Soal12 {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+
+        double c = (double) a;
+
+        System.out.println(c/b);
+    }
+}
