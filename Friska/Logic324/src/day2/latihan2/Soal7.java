ea641fa345f903d7b768af71a803e7dffaf339a3
friskaalfanda <friska.alfanda5@gmail.com>
2023-07-26 23:55:14 +0700

    latihan 2 lanjutan

--- Friska/Logic324/src/day2/latihan2/Soal7.java
+++ Friska/Logic324/src/day2/latihan2/Soal7.java
@@ -14,8 +14,18 @@ public class Soal7 {
         System.out.println("Masukkan lama peminjaman: ");
         int lama = input.nextInt();
 
-        if (lama > 10){
+        if (lama >= 10){
+            int lamadenda = lama - 10;
+            int jumPelajaran = 2000 * pelajaran * lamadenda;
+            int jumnovel =5000 * novel * lamadenda;
+            int jumskripsi = 10000 * skripsi * lamadenda;
+            int totaldenda = jumPelajaran+jumnovel+jumskripsi;
+            System.out.println("Jumlah denda yang harus dibayarkan yaitu sebanyak Rp."+totaldenda);
 
         }
+        else{
+            System.out.println("Anda tidak memiliki denda apapun :)");
+        }
+
     }
 }
