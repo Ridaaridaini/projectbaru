97edf7779d179bf1d4a2cecf7866f4a6c0e90bb7
Rida aridaini <aridainirida@gmail.com>
2023-07-26 10:50:28 +0700

    setelah pembahasan

--- rida/logic324/src/day1/latihan/Soal09.java
+++ rida/logic324/src/day1/latihan/Soal09.java
@@ -4,9 +4,11 @@ public class Soal09 {
     public static void main(String[] args) {
         String a="10";
         String b="3";
-        double intA =Double.valueOf(a);
+        /*double intA =Double.valueOf(a);
         double intB = Double.valueOf(b);
-        System.out.println("Hasil pembagian a dan b ialah: "+intA/intB);
+
+         */
+        System.out.println("Hasil pembagian a dan b ialah: "+Double.valueOf(a)/Double.valueOf(b));
 
     }
 }
