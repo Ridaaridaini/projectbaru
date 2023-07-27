095a8192029b24a4cf5d677b0eb207e8cb8e39e1
irfan afandi <irfan.afandi1998@gmail.com>
2023-07-27 16:53:21 +0700

    latihan3

---
+++ irfan/logic 324/src/day3/latihan3/Soal11.java
@@ -0,0 +1,21 @@
+package day3.latihan3;
+
+import java.util.Scanner;
+
+public class Soal11 {
+    public static void main(String[] args) {
+        int i;
+
+        Scanner inputan = new Scanner(System.in);
+
+        System.out.print("Masukkan angka = ");
+        int j = inputan.nextInt();
+
+        for (i = 0; i < j; i++) {
+
+            System.out.print("@");
+
+            }
+        }
+    }
+
