9354ec58ad771ff1d9a842cdad8c1c16eb29db41
Rida aridaini <aridainirida@gmail.com>
2023-07-27 16:32:29 +0700

    latihanDay3

---
+++ rida/logic324/src/day3/latihan/Soal06.java
@@ -0,0 +1,22 @@
+package day3.latihan;
+
+import java.util.Scanner;
+
+public class Soal06 {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.print("Masukkan jumlah n: ");
+        Integer n= input.nextInt();
+        int a=1;
+        for (int i = 0; i <  n ; i++) {
+            if (i%3==2 && i!=0){
+                System.out.print("* ");
+                a=a+4;
+                continue;
+            }
+            System.out.print(a+" ");
+            a= a+4;
+
+        }
+    }
+}
