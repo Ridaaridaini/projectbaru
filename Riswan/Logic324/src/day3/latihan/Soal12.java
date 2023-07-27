8ae62814b3b6d9965bd2fd73247d08174bcbe6f1
riswandha <riswandhaa22@gmail.com>
2023-07-27 16:52:17 +0700

    Day 3 latihan

---
+++ Riswan/Logic324/src/day3/latihan/Soal12.java
@@ -0,0 +1,22 @@
+package day3.latihan;
+
+import java.util.Scanner;
+
+public class Soal12 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Input nilai n : ");
+        int n = input.nextInt();
+        int angka = 1;
+        for (int i = 1; i <= n ; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+
+
+        }
+    }
+}
