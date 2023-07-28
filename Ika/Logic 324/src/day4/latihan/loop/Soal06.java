34233f894d04e28c006cfc86d63df14fc6f38174
IkaNurOktaviana71 <oktaviaika36@gmail.com>
2023-07-28 19:07:42 +0700

    jawaban latihan nomor 10

--- Ika/Logic 324/src/day4/latihan/loop/Soal06.java
+++ Ika/Logic 324/src/day4/latihan/loop/Soal06.java
@@ -9,33 +9,37 @@ public class Soal06 {
 
         Scanner input = new Scanner(System.in);
         System.out.print("masukan saldo OPO: ");
-        long saldoOpo = input.nextLong();
+        int saldoOpo = input.nextInt();
 
         int diskon = 2;
         int cashback = 10;
         int minimalOrder = 40_000;
         int hargaCup = 18_000;
         int cup=0;
+        int maksimalCashback = 30_000;
+
 
         for (int i = 1; i <= 11 ; i++) {
-            if (i*hargaCup>minimalOrder){
-//                if()
+            if (i*hargaCup > minimalOrder){
+                if(i * hargaCup/2 <= saldoOpo){
+                    cup=i;
+                }
             }
-
         }
 
+        System.out.println("jumlah kopi yang dipesan " + cup);
+        int harga = (cup * hargaCup)/2;
+        System.out.println(harga);
 
+        int casbackHarga = harga/cashback;
 
-
-
-//        for (int i = 0; i < ; i++) {
-//            harga = i * hargaCup;
-//            System.out.println(harga);
-//        }
-
-
-
-
+        if (casbackHarga <= maksimalCashback){
+            int saldoAkhir = saldoOpo - (harga-casbackHarga);
+            System.out.println("sisa saldo " + saldoAkhir);
+        } else {
+            int saldoAkhir = saldoOpo - (harga-maksimalCashback);
+            System.out.println("sisa saldo " + saldoAkhir);
+        }
 
     }
 }
