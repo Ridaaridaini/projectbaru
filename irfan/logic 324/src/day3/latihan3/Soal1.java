2acb949e79f358ac1ccb0ccd54b331e7db25ec48
irfan afandi <irfan.afandi1998@gmail.com>
2023-07-28 17:03:40 +0700

    Latihan2

--- irfan/logic 324/src/day3/latihan3/Soal1.java
+++ irfan/logic 324/src/day3/latihan3/Soal1.java
@@ -6,15 +6,15 @@ public class Soal1 {
     public static void main(String[] args) {
 
         int i, n;
-        int a=1;
+        int a = 1;
         Scanner inputan = new Scanner(System.in);
 
         System.out.print("Masukkan angka = ");
         n = inputan.nextInt();
 
-        for(i=0; i<n; i++){    //
-            System.out.print(a + " ");
-            a= a+2;
+        for(i=0; i<n; i++){                 //0
+            System.out.print(a + " ");      //1
+            a = a+2;
         }
         System.out.println();
     }
