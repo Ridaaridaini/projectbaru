26d46cd24f861c4cb751669aeb3721a3496b7af8
IkaNurOktaviana71 <oktaviaika36@gmail.com>
2023-07-29 10:47:57 +0700

    update jawaban nomor 07

--- Ika/Logic 324/src/day4/latihan/loop/Soal10.java
+++ Ika/Logic 324/src/day4/latihan/loop/Soal10.java
@@ -2,10 +2,17 @@ package day4.latihan.loop;
 
 import java.util.Scanner;
 
-public class Soal06 {
+public class Soal10 {
     public static void main(String[] args) {
-        // sekelompok orang ingin membeli es kopi susu dengan menggunakan cashback 50%
-        // (maksimal diskon Rp 100000 dengan meinimal oerder Rp40.000) ditambah ca
+        // sekelompok orang ingin membeli es kopi susu dengan menggunakan promo diskon 50%
+        // (maksimal diskon Rp 100.000 minimal order Rp 40.000) ditambahkan cashback 10%
+        // cashback dari harga akhir yang dibayarkan maksimal cashback Rp 30.000
+        // berapkah cup yang bisa dibeli jika mereka tidak ingin membayar lebih besar
+        // dari nominal diskonnya? dan berapa saldo akhirnya setelah order diterima oleh customer
+        // input : saldo OPO
+        // contoh : saldo OPO =Rp 60.000
+        // output : jumlah cup = 6, saldo akhir = Rp 11.400
+        // harga kopi 18.000 ditambah ca
 
         Scanner input = new Scanner(System.in);
         System.out.print("masukan saldo OPO: ");
@@ -21,7 +28,7 @@ public class Soal06 {
 
         for (int i = 1; i <= 11 ; i++) {
             if (i*hargaCup > minimalOrder){
-                if(i * hargaCup/2 <= saldoOpo){
+                if(i * hargaCup/diskon <= saldoOpo){
                     cup=i;
                 }
             }
