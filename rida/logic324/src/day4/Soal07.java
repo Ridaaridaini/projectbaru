1365e6a04360790c82def741732689fe87d9a698
Rida aridaini <aridainirida@gmail.com>
2023-07-30 23:36:52 +0700

    update07

--- rida/logic324/src/day4/Soal07.java
+++ rida/logic324/src/day4/Soal07.java
@@ -13,7 +13,9 @@ public class Soal07 {
         String kotakB= "B";
         int kartuUser= kartuKomputer;
         int jumlahKartu=0;
-        while(jumlahKartu== kartuUser + kartuKomputer){
+        int n=kartuKomputer*2;
+        //System.out.println(jumlahKartu+" "+kartuKomputer+" "+kartuUser+" "+kotakA+" "+kotakB);
+        do {
             Random randomGeneerator = new Random();
             System.out.print("masukkan jumlah tawaran: ");
             Integer m= input.nextInt();
@@ -29,21 +31,32 @@ public class Soal07 {
                     System.out.println("You Win");
                     System.out.println("angka pada kartu A "+intA +" > " +intB);
                     System.out.println("Sisa kartu anda "+kartuUser+" Sisa kartu lawan "+kartuKomputer);
+                    if (kartuKomputer > kartuUser){
+                        jumlahKartu= kartuKomputer;
+                    } else {
+                        jumlahKartu= kartuUser;
+                    }
                 } else if (intA < intB) {
                     kartuUser= kartuUser-m;
                     kartuKomputer= kartuKomputer+m;
                     System.out.println("You Lose");
                     System.out.println("angka pada kartu A "+intA +" < " +intB);
                     System.out.println("Sisa kartu anda "+kartuUser+" Sisa kartu lawan "+kartuKomputer);
+                    if (kartuKomputer > kartuUser){
+                        jumlahKartu= kartuKomputer;
+                    } else {
+                        jumlahKartu= kartuUser;
+                    }
                 }else {
                     System.out.println("angka pada kartu A "+intA +" sama dengan " +intB);
                     System.out.println("you are Balanced");
+                    if (kartuKomputer > kartuUser){
+                        jumlahKartu= kartuKomputer;
+                    } else {
+                        jumlahKartu= kartuUser;
+                    }
                 }
-                if (kartuKomputer > kartuUser){
-                    jumlahKartu= kartuKomputer;
-                } else {
-                    jumlahKartu= kartuUser;
-                }
+                System.out.println(jumlahKartu);
                 continue;
             }
             if (kotak.equalsIgnoreCase(kotakB)){
@@ -55,24 +68,35 @@ public class Soal07 {
                     System.out.println("You Lose");
                     System.out.println("angka pada kartu A "+intB +" < " +intA);
                     System.out.println("Sisa kartu anda "+kartuUser+" Sisa kartu lawan "+kartuKomputer);
+                    if (kartuKomputer > kartuUser){
+                        jumlahKartu= kartuKomputer;
+                    } else {
+                        jumlahKartu= kartuUser;
+                    }
                 } else if (intA < intB) {
                     kartuUser= kartuUser+m;
                     kartuKomputer= kartuKomputer-m;
                     System.out.println("You Win");
                     System.out.println("angka pada kartu A "+intB +" > " +intA);
                     System.out.println("Sisa kartu anda "+kartuUser+" Sisa kartu lawan "+kartuKomputer);
+                    if (kartuKomputer > kartuUser){
+                        jumlahKartu= kartuKomputer;
+                    } else {
+                        jumlahKartu= kartuUser;
+                    }
                 }else {
                     System.out.println("angka pada kartu A "+intA +" sama dengan " +intB);
                     System.out.println("you are Balanced");
+                    if (kartuKomputer > kartuUser){
+                        jumlahKartu= kartuKomputer;
+                    } else {
+                        jumlahKartu= kartuUser;
+                    }
                 }
-                if (kartuKomputer > kartuUser){
-                    jumlahKartu= kartuKomputer;
-                } else {
-                    jumlahKartu= kartuUser;
-                }
+                System.out.println(jumlahKartu);
                 continue;
             }
-        }
+        }while(jumlahKartu == n);
 
 
 
