2e86f8aaa2916ce85e7a2f1763aa29d3110c7aea
Dianty99 <diantyfistikaap@gmail.com>
2023-07-30 22:16:14 +0700

    UPDATE JAWABAN DAY4

--- Danty/Logic324/src/day4/Soal06.java
+++ Danty/Logic324/src/day4/Soal06.java
@@ -7,6 +7,37 @@ public class Soal06 {
 
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Saldo OPO : ");
+        int saldo = input.nextInt();
+
+        int hargaKopi = 9000; //harga per cup setelah di diskon
+       int cashBack = 10; //10%
+
+        do {
+            if (saldo >= 54000) {
+                if (saldo / hargaKopi <= 11) {
+                    int cup = (saldo / hargaKopi);
+                    int hargaBeliKopi = (cup * hargaKopi);
+                    int saldo2 = saldo - hargaBeliKopi;
+                    int sisaSaldo = saldo2 + (hargaBeliKopi / cashBack);
+                    System.out.println("Jumlah cup : " + cup + " Saldo Akhir = Rp " + sisaSaldo);
+
+                } else if (saldo / hargaKopi > 11) {
+                    int cup = 11;
+                    int hargaBeliKopi = (cup * hargaKopi);
+                    int saldo2 = saldo - hargaBeliKopi;
+                    int sisaSaldo = saldo2 + (hargaBeliKopi / cashBack);
+                    System.out.println("Jumlah cup :" + cup + "Saldo Akhir = Rp " + sisaSaldo);
+                }
+                break;
+
+            }
+        } while (saldo >= 54000);
+
+
+
 
 
     }
