4505efb06ad6ec9ac698a2593ea2b1a42d7b2fb2
riswandha <riswandhaa22@gmail.com>
2023-07-31 16:22:17 +0700

    Day 4 latihan

--- Riswan/Logic324/src/day5/ArraySorting.java
+++ Riswan/Logic324/src/day5/ArraySorting.java
@@ -1,15 +1,26 @@
 package day5;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 public class ArraySorting {
     public static void main(String[] args) {
-        int[] arr = {1,5,3,9,7};
+        Integer[] arr = {1,5,3,9,7};
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
+
+        //ascending
         Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        //decesding
+        Arrays.sort(arr, Collections.reverseOrder());
 
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
