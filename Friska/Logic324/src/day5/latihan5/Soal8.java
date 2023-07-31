06156cc5f28b968e1261a6d5c131eaaf0b625bb8
friskaalfanda <friska.alfanda5@gmail.com>
2023-07-31 22:19:56 +0700

    latihan5

--- Friska/Logic324/src/day5/latihan5/Soal8.java
+++ Friska/Logic324/src/day5/latihan5/Soal8.java
@@ -1,4 +1,37 @@
 package day5.latihan5;
 
+import java.util.Scanner;
+
 public class Soal8 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan panjang deret");
+        int panjang = input.nextInt();
+        int a = 1;
+        int b = 0;
+
+        int [] arrA = new int [panjang];
+        System.out.print("Deret Genap: ");
+        for (int i = 0; i < panjang; i++) {
+            System.out.print( b + " ");
+            arrA[i] = b;
+            b = b + 2;
+
+        }System.out.println();
+
+        System.out.print("Deret Ganjil: ");
+        for (int i = 0; i < panjang; i++) {
+            System.out.print( a + " " );
+            arrA[i] = a;
+            a = a + 2 ;
+
+        }
+//        for (int i = 0; i < ; i++) {
+
+        }
+
+
+
+    }
+
 }
