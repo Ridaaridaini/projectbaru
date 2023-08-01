2476ed9dccd790a684a2e883fd2c8ac068144f98
Dianty99 <diantyfistikaap@gmail.com>
2023-08-01 16:53:13 +0700

    UPDATE JAWABAN DAY6

--- Danty/Logic324/src/day6/latihanday6/Soal07.java
+++ Danty/Logic324/src/day6/latihanday6/Soal07.java
@@ -9,7 +9,23 @@ public class Soal07 {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Kalimat : ");
-        String kalimat = input.nextLine();
+        String kalimat = input.nextLine();//Halo Apa Kabarmu?
+
+        kalimat = kalimat.replace(" ","");//HaloApaKabarmu?
+        kalimat= kalimat.toLowerCase();//
+
+        String unik = "";
+
+        String [] arrKalimat = kalimat.split("");
+        for (int i = 0; i < arrKalimat.length; i++) {
+            String huruf = arrKalimat[i];
+            if(!unik.contains(huruf)){
+                unik = unik + huruf;
+            }
+
+        }
+        System.out.println(unik);
+        System.out.println(unik.length());
 
 
 
--- Danty/Logic324/src/day6/latihanday6/Soal08.java
+++ Danty/Logic324/src/day6/latihanday6/Soal08.java
@@ -1,4 +1,8 @@
 package day6.latihanday6;
 
 public class Soal08 {
+
+    public static void main(String[] args) {
+
+    }
 }
