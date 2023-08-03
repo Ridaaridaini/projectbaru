35814698d6ca938415c5b1ad38c18b0b84b427a5
Bagus Iman Prasetyo <bagus.i.p.bipbip@gmail.com>
2023-08-04 06:14:44 +0700

    updated

--- bagus/logic324/src/latihanA/Soal2B.java
+++ bagus/logic324/src/latihanA/Soal2B.java
@@ -18,68 +18,74 @@ public class Soal2B {
 
         for (int i = 1; i < ruteSplit.length; i++) {
 //            System.out.println(ruteSplit[i]);
-            String jarakAntar = ruteSplit[i];
-            if (jarakAntar.equals("tempat1") && ruteSplit[i - 1].equals("toko")) {
-                jarak = jarak + 2;
-            } else if (jarakAntar.equals("tempat2") && ruteSplit[i - 1].equals("toko")) {
-                jarak = jarak + 2.5;
-            } else if (jarakAntar.equals("tempat3") && ruteSplit[i - 1].equals("toko")) {
-                jarak = jarak + 4;
-            } else if (jarakAntar.equals("tempat4") && ruteSplit[i - 1].equals("toko")) {
-                jarak = jarak + 6.5;
+
+            if (ruteSplit[i].equals("tempat1")) {
+                if (ruteSplit[i - 1].equalsIgnoreCase("tempat2")) {
+                    jarak = jarak + 0.5;
+                } else if (ruteSplit[i - 1].equalsIgnoreCase("tempat3")) {
+                    jarak = jarak + 2;
+                } else if (ruteSplit[i - 1].equalsIgnoreCase("tempat4")) {
+                    jarak = jarak + 4.5;
+                } else if (ruteSplit[i - 1].equalsIgnoreCase("toko")) {
+                    jarak = jarak + 2;
+                }
+            } else if (ruteSplit[i].equalsIgnoreCase("tempat2")) {
+
+                if (ruteSplit[i - 1].equalsIgnoreCase("tempat3")) {
+                    jarak = jarak + 1.5;
+                } else if (ruteSplit[i - 1].equalsIgnoreCase("tempat4")) {
+                    jarak = jarak + 4;
+                } else if (ruteSplit[i - 1].equalsIgnoreCase("tempat1")) {
+                    jarak = jarak + 0.5; // jarak = 2 + 0.5 = 2.5
+                } else if (ruteSplit[i - 1].equalsIgnoreCase("toko")) {
+                    jarak = jarak + 2.5;
+                }
+
+            } else if (ruteSplit[i].equalsIgnoreCase("tempat3")) {
+
+                if (ruteSplit[i - 1].equalsIgnoreCase("toko")) {
+                    jarak = jarak + 4;
+                } else if (ruteSplit[i - 1].equalsIgnoreCase("tempat1")) {
+                    jarak = jarak + 2;
+                } else if (ruteSplit[i - 1].equalsIgnoreCase("tempat2")) {
+                    jarak = jarak + 1.5;
+                } else if (ruteSplit[i - 1].equalsIgnoreCase("tempat4")) {
+                    jarak = jarak + 2.5;
+                }
+
+            } else if (ruteSplit[i].equalsIgnoreCase("tempat4")) {
+
+                if (ruteSplit[i - 1].equalsIgnoreCase("toko")) {
+                    jarak = jarak + 6.5;
+                } else if (ruteSplit[i - 1].equalsIgnoreCase("tempat1")) {
+                    jarak = jarak + 4.5;
+                } else if (ruteSplit[i - 1].equalsIgnoreCase("tempat2")) {
+                    jarak = jarak + 4;
+                } else if (ruteSplit[i - 1].equalsIgnoreCase("tempat3")) {
+                    jarak = jarak + 2.5;
+                }
+
+            } else if (ruteSplit[i].equalsIgnoreCase("toko") && i > 0) {
+
+                if (ruteSplit[i - 1].equalsIgnoreCase("tempat1")) {
+                    jarak = jarak + 2;
+                } else if (ruteSplit[i - 1].equalsIgnoreCase("tempat2")) {
+                    jarak = jarak + 2.5;
+                } else if (ruteSplit[i - 1].equalsIgnoreCase("tempat3")) {
+                    jarak = jarak + 4;
+                } else if (ruteSplit[i - 1].equalsIgnoreCase("tempat4")) {
+                    jarak = jarak + 6.5;
+                }
+
             }
-//            if (jarakAntar.equals("tempat2")) {
-//                jarak = jarak + 0.5;
-//            }
-
-
-//            if (jarakAntar.equals("tempat1") && ruteSplit[i + 1].equals("tempat2")) {
-//                jarak = jarak + 0.5;
-//            }
-//            if (jarakAntar.equals("tempat2") && ruteSplit[i+1].equals("tempat3")){
-//                jarak = jarak + 1.5;
-//            }
-//            if (jarakAntar.equals("tempat3") && ruteSplit[i+1].equals("tempat4")){
-//                jarak = jarak + 2.5;
-//            }
-//            if (jarakAntar.equals("tempat1") && ruteSplit[i + 1].equals("tempat3")) {
-//                jarak = jarak + 2;
-//            }
-//            if (jarakAntar.equals("tempat1") && ruteSplit[i + 1].equals("tempat4")) {
-//                jarak = jarak + 4.5;
-//            }
-//            if (jarakAntar.equals("tempat2") && ruteSplit[i - 1].equals("toko")) {
-//                jarak = jarak + 2.5;
-//            }
-//            if (jarakAntar.equals("tempat3") && ruteSplit[i - 1].equals("toko")) {
-//                jarak = jarak + 4.0;
-//            }
-//            if (jarakAntar.equals("tempat4") && ruteSplit[i - 1].equals("toko")) {
-//                jarak = jarak + 7.0;
-//            }
-//            if (jarakAntar.equals("tempat2")) {
-//                jarak = jarak + 0.5;
-//            }
-//            if (jarakAntar.equals("tempat3")) {
-//                jarak = jarak + 1.5;
-//            }
-////            if (jarakAntar.equals("tempat3") && ruteSplit[i - 1].equals("tempat4")) {
-////
-////            }
-//            if (jarakAntar.equals("tempat4") && ruteSplit[i - 1].equals("toko")) {
-//                jarak = jarak + 7.0;
-//            }
-            if (jarakAntar.equals("toko")) {
-                jarak = jarak + jarak;
-            }
-
         }
-
         double bensin = jarak / 2.5;
 
         System.out.println(jarak);
         System.out.println(bensin + " liter");
+
     }
+
 }
 
 
