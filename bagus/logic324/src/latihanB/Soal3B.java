4a488d4f8aa780aeccbb21bc86cc238d01eb0804
Bagus Iman Prasetyo <bagus.i.p.bipbip@gmail.com>
2023-08-04 06:37:30 +0700

    updated ter ter updated

--- bagus/logic324/src/latihanB/Soal3B.java
+++ bagus/logic324/src/latihanB/Soal3B.java
@@ -5,53 +5,63 @@ import java.util.Scanner;
 public class Soal3B {
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("masukan n: ");
-        int n = input.nextInt();
-        int a = 100;
-        int angka = 100;
-        int angkaDitemukan = 0;
+        Scanner scanner = new Scanner(System.in);
 
-        while (angkaDitemukan < n) {
+        System.out.print("input n: ");
+        int n = scanner.nextInt();
 
-            for (; angka > 10; ) {
+        if (!(n>=1 && n<=100)){
+            System.out.println("nilai n harus antara 1 dan 100");
+            System.exit(0);
+        }
 
+        int angka = 100, count = 0;
 
-                String strA = String.valueOf(angka);
-                String[] sSplit = strA.split("");
+        while (angka>=100){
 
-                double hasilTambah = 0;
-//                String hasilTambahStr = String.valueOf(hasilTambah);
-//                String[] hasilTambahSplit = hasilTambahStr.split("");
-//                double hasilTambahTambah = 0;
+            String strAngka = String.valueOf(angka);
+            String[] arrAngka = strAngka.split("");
+            Double[] singleAngka = new Double[arrAngka.length];
 
-                for (int i = 0; i < sSplit.length; i++) {
-                    int sParsing = Integer.parseInt(sSplit[i]);
-                    hasilTambah = hasilTambah + Math.pow(sParsing, 2);
-                }
-                if (angka == 1){
-                    angkaDitemukan++;
+            double total = 0;
+            for (int i = 0; i < arrAngka.length; i++) {
+                singleAngka[i] = Double.parseDouble(arrAngka[i]);
 
-                    System.out.println(a +"adalah si angka 1");
-
-                    a++;
-
-                    angka = a;
-
-                    break;
-
-                } else if (angka > 10) {
-                    angka = (int) hasilTambah;
-
-                } else {
-                    a++;
-                    angka = a;
-                }
-
-                System.out.println(a);
-                System.out.println(hasilTambah);
+                total = total + Math.pow(singleAngka[i], 2.0);
             }
 
+            if (total == 1) {
+                count++;
+                System.out.println(angka + " adalah \"Si Angka 1\"");
+
+                if (count==n){
+                    System.exit(0);
+                }
+            }
+
+            while (total >= 10){
+                int intTotal = (int)total;
+                String strTotal = String.valueOf(intTotal);
+                String[] arrTotal = strTotal.split("");
+                Double[] singleTotal = new Double[arrTotal.length];
+                total = 0;
+
+                for (int i = 0; i < arrTotal.length; i++) {
+                    singleTotal[i] = Double.parseDouble(arrTotal[i]);
+
+                    total = total + Math.pow(singleTotal[i], 2.0);
+                }
+                if (total == 1) {
+                    count++;
+                    System.out.println(angka + " adalah \"Si Angka 1\"");
+
+                    if (count==n){
+                        System.exit(0);
+                    }
+                }
+            }
+
+            angka++;
         }
     }
 }
