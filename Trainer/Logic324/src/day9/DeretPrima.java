68f32f1a99d65936b0e50bc56314bce59d28905d
yuhan <yuhan@gmail.com>
2023-08-04 09:10:58 +0700

    Pembahasan Day 9

---
+++ Trainer/Logic324/src/day9/DeretPrima.java
@@ -0,0 +1,27 @@
+package day9;
+
+public class DeretPrima {
+    public static void main(String[] args) {
+        int n = 5;
+        //Prima : 2 3 5 7 11
+
+        int[] arrPrima = new int[n];
+
+        int a = 2; //bilangan yang dicek
+        for (int i = 0; i < arrPrima.length;) {
+            //i buat penanda berapa bil prima yg udah ketemu
+            int cekPrima = 0;
+            for (int j = 1; j <= a; j++) {
+                //j untuk pengecek ada berapa angka yang bisa habis membagi a
+                if (a % j == 0){
+                    cekPrima = cekPrima + 1;
+                }
+            }
+            if (cekPrima == 2){
+                arrPrima[i] = a;
+                i++;
+            }
+            a++;
+        }
+    }
+}
