8208ad50f70a8db030990388436a396d64ede9fe
irfan afandi <irfan.afandi1998@gmail.com>
2023-08-04 08:17:09 +0700

    latihan8

---
+++ irfan/logic 324/src/latihan/Keranjang.java
@@ -0,0 +1,34 @@
+package latihan;
+
+import java.util.Scanner;
+
+public class Keranjang {
+    public static void main(String[] args) {
+        int keranjang1, keranjang2, keranjang3, kepasar;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print(" keranjang 1 = ");
+        keranjang1 = input.nextInt();
+
+        System.out.print(" keranjang 2 = ");
+        keranjang2 = input.nextInt();
+
+        System.out.print(" keranjang 3 = ");
+        keranjang3 = input.nextInt();
+
+        System.out.print(" dibawa kepasar = ");
+        kepasar = input.nextInt();
+
+        if(kepasar == 1 ){
+            System.out.println("jumlah buah = " + (keranjang2 + keranjang3 ));
+        } else if (kepasar == 2) {
+            System.out.println("jumlah buah = " + (keranjang1 + keranjang3));
+        } else if (kepasar == 3) {
+            System.out.println("jumlah buah = " + (keranjang1 + keranjang2));
+        }else {
+            System.out.println("error");
+        }
+
+    }
+}
---
+++ irfan/logic 324/src/latihan/Soal5.java
@@ -0,0 +1,58 @@
+package latihan;
+
+import java.util.Scanner;
+
+public class Soal5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String inputan = scanner.nextLine();
+
+        String[] arrA = inputan.split(";"); // arrA ["Laki-laki dewasa = 3 orang" ]
+
+        double jumlah = 0;
+        int jumlahOrang = 0;
+        
+        for (int i = 0; i < arrA.length; i++) {
+        String[] arrB = arrA[i].replace("-","").split("="); // arrB[i]= ["Lakilaki dewasa" , "= 3 orang" ]
+        int orang = Integer.parseInt(arrB[1].replace(" ","").replace("orang","")); // [3]
+        
+            if("lakilakidewasa".equalsIgnoreCase(arrB[0].replace(" ","").toLowerCase())){
+                jumlah = jumlah + (orang * 2); // menghitung porsi
+                jumlahOrang = jumlahOrang + orang; // menghitung orang
+            
+            } else if ("perempuandewasa".equalsIgnoreCase(arrB[0].replace(" ","").toLowerCase())) {
+                jumlah = jumlah + orang; // porsi
+                jumlahOrang = jumlahOrang + orang;
+            } else if ("remaja".equalsIgnoreCase(arrB[0].replace(" ","").toLowerCase())) {
+                jumlah = jumlah + orang;
+                jumlahOrang = jumlahOrang + orang;
+            } else if ("anakanak".equalsIgnoreCase(arrB[0].replace(" ","").toLowerCase())) {
+                jumlah = jumlah + orang * 0.5;
+                jumlahOrang = jumlahOrang + orang;
+                
+            } else if ("balita".equalsIgnoreCase(arrB[0].replace(" ","").toLowerCase())) {
+                jumlah = jumlah + orang;
+                jumlahOrang = jumlahOrang + orang;
+            }
+
+        }
+
+        for (int i = 0; i < arrA.length; i++) {
+            String[] arrB = arrA[i].replace("-","").split("="); // arrB[i]= ["Lakilaki dewasa" , "= 3 orang" ]
+            int orang = Integer.parseInt(arrB[1].replace(" ","").replace("orang","")); // [3]
+
+            if (jumlahOrang>5 && jumlahOrang%2!=0){
+
+                if ("perempuandewasa".equalsIgnoreCase(arrB[0].replace(" ","").toLowerCase())) {
+                    jumlah = jumlah + orang;
+                }
+
+            }
+        }
+            System.out.println(jumlahOrang+" orang");
+            System.out.println(jumlah+" porsi");
+
+
+    }
+}
