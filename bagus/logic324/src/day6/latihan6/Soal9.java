87707a58511699ee63f302215304649150dcee64
Bagus Iman Prasetyo <bagus.i.p.bipbip@gmail.com>
2023-08-07 16:49:14 +0700

    updated

--- bagus/logic324/src/day6/latihan6/Soal9.java
+++ bagus/logic324/src/day6/latihan6/Soal9.java
@@ -1,7 +1,39 @@
 package day6.latihan6;
 
+import java.util.Scanner;
+
 public class Soal9 {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("masukan kata pertama: ");
+        String kataSatu = input.nextLine();
+        String[] kataSatuSplit = kataSatu.split("");
+
+        System.out.println("masukan kata kedua: ");
+        String kataDua = input.nextLine();
+        String[] kataDuaSplit = kataDua.split("");
+
+        String bandingBandingke = "";
+        String bandingBandingke1 = "";
+
+        for (int i = 0; i < kataSatuSplit.length; i++) {
+            for (int j = 0; j < kataSatuSplit.length; j++) {
+                bandingBandingke = kataSatuSplit[i];
+                bandingBandingke1 = kataDuaSplit[i];
+
+
+
+//                if (kataSatuSplit[i].equals(kataDuaSplit[i])) {
+//                    System.out.println("Yes");
+//                } else {
+//                    System.out.println("No");
+//                }
+            }
+            System.out.print(bandingBandingke);
+
+            System.out.print(bandingBandingke1);
+        }
     }
 }
