cd66928e850ba171ba78114f3165fd108fa028ef
Rida aridaini <aridainirida@gmail.com>
2023-08-07 16:56:17 +0700

    day10

--- rida/logic324/src/day6/latihan/Soal09.java
+++ rida/logic324/src/day6/latihan/Soal09.java
@@ -1,7 +1,25 @@
 package day6.latihan;
 
+import java.util.Scanner;
+
 public class Soal09 {
     public static void main(String[] args) {
-
+        Scanner input= new Scanner(System.in);
+        int d=0;
+        System.out.println("Masukkan kata ke-1");
+        String a=input.nextLine().toLowerCase();
+        System.out.println("Masukkan kata ke-2");
+        String[] b=input.nextLine().toLowerCase().split("");
+        for (int i = 0; i < b.length; i++) {
+            String c=b[i];
+            if (a.contains(c)){
+               d=d+1;
+            }
+        }
+        System.out.println();
+        if (d!=0){
+            System.out.println("YES");
+        }else
+            System.out.println("NO");
     }
 }
