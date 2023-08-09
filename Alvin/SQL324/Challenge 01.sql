806228a8b1e7a752edd1126e86219d3d88f0a070
Aslamadin Alvian Haz <alvianaslamadin@gmail.com>
2023-08-09 16:50:07 +0700

    Update Challenge 01.sql

--- Alvin/SQL324/Challenge 01.sql
+++ Alvin/SQL324/Challenge 01.sql
@@ -48,10 +48,28 @@ select artis.nm_artis as "Actor", film.nm_film as "Film", genre.nm_genre as "Gen
 	inner join genre
 	on film.genre = genre.kd_genre
 	where genre.nm_genre ilike 'drama';
+	
+-- 21. tampilkan nama artis, film dan genre yang bermain film dengan genre horor
+select artis.nm_artis as "Nama Artis", film.nm_film as "Nama Film", genre.nm_genre as "Genre"
+	from artis
+	join film
+		on artis.kd_artis = film.artis
+	join genre
+		on film.genre = genre.kd_genre
+	where genre.nm_genre ilike 'action';
 
+-- 22. tampilkan nama film dan artis yang dibintangi oleh artis yang huruf depnya 'J'
+select film.nm_film as "Nama Film", artis.nm_artis as "Nama Film"
+	from film
+	join artis
+		on film.artis = artis.kd_artis
+	where artis.nm_artis ilike 'j%';
 
+-- 23. tampilkan nama artis yang paling banyak bermain film
+select artis.nm_artis, film.nm_film
+	from film
+	join artis
+		on film.artis = artis.kd_artis;
 
-
-
-
-
+-- 24. tampilkan negara mana yang paling banyak filmnya
+-- 25. tampilkan data negara dengan jumlah filmnya
